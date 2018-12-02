fa0477a68dea285300b7b88a109afe085c300fe0
Abdulhakeem Adetunji Mustapha <abdulhakeemmustapha@gmail.com>
2018-12-02 20:27:04 +0100

    Added form validation and show request status

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,31 +1,33 @@
 <template>
   <div>
     <h2>Form</h2>
-     <b-field :label="field.field" v-for="(field, key) in insuranceDetail.field_set"
-      :key="key">
-        <b-select :placeholder="field.field" v-if="field.type === 'select'"
-          v-model="submission[field.id]" expanded>
-            <option v-for="(option, key) in field.option_set" :key="key" :value="option.id">
-              {{ option.display }}
-            </option>
-        </b-select>
-        <b-datepicker
-            :placeholder="field.field" icon="calendar-today" v-model="submission[field.id]"
-            v-else-if="field.type === 'date'">
-        </b-datepicker>
-        <div class="block" v-else-if="field.type === 'radio'">
-          <b-radio  v-for="(option, key) in field.option_set" :key="key"
-            v-model="submission[field.id]"  :native-value="option.id">{{option.display}}</b-radio>
-        </div>
-        <div class="block" v-else-if="field.type === 'checkbox'">
-          <b-checkbox  v-for="(option, key) in field.option_set" :key="key"
-            v-model="submission[field.id]"  :native-value="option.id">{{option.display}}</b-checkbox>
-        </div>
+    <form method="post" @submit.prevent="submit">
+      <b-field :label="field.field" v-for="(field, key) in insuranceDetail.field_set"
+        :message="field.message" :key="key">
+          <b-select :placeholder="field.field" v-if="field.type === 'select'"
+            v-model="submission[field.id]" expanded>
+              <option v-for="(option, key) in field.option_set" :key="key" :value="option.id">
+                {{ option.display }}
+              </option>
+          </b-select>
+          <b-datepicker
+              :placeholder="field.field" icon="calendar-today" v-model="submission[field.id]"
+              v-else-if="field.type === 'date'">
+          </b-datepicker>
+          <div class="block" v-else-if="field.type === 'radio'">
+            <b-radio  v-for="(option, key) in field.option_set" :key="key"
+              v-model="submission[field.id]"  :native-value="option.id">{{option.display}}</b-radio>
+          </div>
+          <div class="block" v-else-if="field.type === 'checkbox'">
+            <b-checkbox  v-for="(option, key) in field.option_set" :key="key"
+              v-model="submission[field.id]"  :native-value="option.id">{{option.display}}</b-checkbox>
+          </div>
 
-        <b-input v-model="submission[field.id]" :pattern="field.validation"
-          :type="field.type" :placeholder="field.field" v-else></b-input>
+          <b-input v-model="submission[field.id]" :pattern="field.validation" :title="field.message"
+            :type="field.type" :placeholder="field.field" :required="field.is_required" v-else></b-input>
       </b-field>
-      <button class="button is-primary" @click.prevent="submit">Submit</button>
+      <button class="button is-primary">Submit</button>
+    </form>
   </div>
 </template>
 
@@ -34,9 +36,9 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
+      isRequired: true,
       insuranceDetail: {},
-      submission: {},
-      t: []
+      submission: {}
     }
   },
   mounted () {
@@ -49,10 +51,20 @@ export default {
         }
       }
       this.insuranceDetail = data.data
+    }).catch(err => {
+      loader.close()
+      this.$toast.open({
+        duration: 5000,
+        message: `Unable to fetch risk informaiton`,
+        position: 'is-bottom',
+        type: 'is-danger'
+      })
+      this.$router.push('/')
     })
   },
   methods: {
     submit () {
+      const loader = this.$loading.open()
       const params = new URLSearchParams()
       params.append('risk', this.$route.params.id)
       const fields = []
@@ -64,15 +76,25 @@ export default {
       }
       params.append('submissionvalue_set', fields)
       console.log(JSON.stringify(fields))
-      this.$axios.post(`http://localhost:8000/insurer/submit/`,
-        {
-          'risk': this.$route.params.id,
-          'submissionvalue_set': fields
-        }).then(response => {
-          window.alert('Form has been submitted')
-          this.submission = {}  
+      this.$axios.post(`http://localhost:8000/insurer/submit/`, {
+        'risk': this.$route.params.id,
+        'submissionvalue_set': fields
+      }).then(response => {
+        this.$toast.open({
+          message: 'Form has been submitted!',
+          type: 'is-success',
+          position: 'is-bottom'
+        })
+        this.submission = {}
+        loader.close()
       }).catch(err => {
-        console.log(err.message)
+        loader.close()
+        this.$toast.open({
+          duration: 5000,
+          message: `Error occured while creating risk`,
+          position: 'is-bottom',
+          type: 'is-danger'
+        })
       })
     }
   }
