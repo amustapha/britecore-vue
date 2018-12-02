38162a9a82f6636b529791f02cdff8a39c3cc17c
Abdulhakeem Adetunji Mustapha <abdulhakeemmustapha@gmail.com>
2018-12-02 20:28:28 +0100

    Add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <template>
-  <div id="app" class="section">
-    <router-view/>
+  <div id="app">
+    <nav class="navbar is-info" role="navigation" aria-label="main navigation" id="navbarBasicExample">
+        <div class="navbar-brand">
+          <router-link to="/" class="navbar-item" tag="b">BRITECORE</router-link>
+
+          <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <router-link to="/" class="navbar-item">Risks</router-link>
+            <router-link to="/new-risk" class="navbar-item">New Risk</router-link>
+          </div>
+        </div>
+      </nav>
+      <div class="section">
+        <router-view/>
+      </div>
   </div>
 </template>
 
